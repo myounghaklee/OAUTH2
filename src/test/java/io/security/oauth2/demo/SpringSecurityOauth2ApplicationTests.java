db06b2e388241f2410583538dec126e653d8d265
myounghak <devhak.enzo@gmail.com>
2024-01-29 18:52:07 +0900

    first commits

---
+++ src/test/java/io/security/oauth2/demo/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
